f1bf147b532e94855f8fc7b24bf46eab7d61a39b
cfcz <1109318719@qq.com>
2024-09-13 07:40:32 +0800

    return initial commit

--- middleware.ts
+++ middleware.ts
@@ -2,9 +2,11 @@ import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
 const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)'])
 
-// export default clerkMiddleware()
-
-export default clerkMiddleware()
+export default clerkMiddleware((auth, request) => {
+  if (!isPublicRoute(request)) {
+    auth().protect()
+  }
+}, { debug: true })
 
 export const config = {
   matcher: [
